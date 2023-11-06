600f925f81c027f04efcb0b8ccb3a6f92bdc5276
Luis Aguilar <laz133@example.com>
2023-11-06 17:58:02 -0500

    Creando rss feed y suscripcion

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name ='blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    # path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-        # '<int:id>/', 
         views.post_detail,
         name='post_detail'),
     path('<int:post_id>/share/',
@@ -16,4 +15,5 @@ urlpatterns = [
          views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/',
          views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
